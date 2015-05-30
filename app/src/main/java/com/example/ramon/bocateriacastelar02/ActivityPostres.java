eb9104a5a0f86ee64808c4e707c03f657e2eb599
borjanp92 <bnunez.renacens@gmail.com>
2015-05-30 18:58:35 +0200

    ActivityPostre --> hecho

--- app/src/main/java/com/example/ramon/bocateriacastelar02/ActivityPostres.java
+++ app/src/main/java/com/example/ramon/bocateriacastelar02/ActivityPostres.java
@@ -1,7 +1,9 @@
 package com.example.ramon.bocateriacastelar02;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,23 +11,42 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
 import dao.Conexion;
 import dto.Pedido;
 import dao.SingletonMesa;
 import dto.Producto;
 
-/**
- * Created by RAMON on 19/01/2015.
- */
 public class ActivityPostres extends Activity {
-    private Conexion conexion;
+    final static String COD_ENVIO="NEWPROD";
+    final static String LOGCAT = "ALTA";
+    private List<Producto> productos;
+    private String ipServidor;
+    private int puerto;
+    private Socket socket;
+    private ComidaTask postreTask;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+
+    private void inicializa() {
+        ipServidor = "192.168.60.10";
+        puerto = 40001;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.productos);
-        conexion = new Conexion(this,"conexion",null,1);
+        inicializa();
+        postreTask = new ComidaTask();
+        postreTask.execute();
+
 
         crearAdapterPostres();
     }
@@ -50,14 +71,15 @@ public class ActivityPostres extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private ArrayList<Producto> obtenerPostres(){
-        String[] descripciones=getResources().getStringArray(R.array.postresDescripcion);
+    private List<Producto> obtenerPostres(){
+        String[] codPostres=getResources().getStringArray(R.array.codProductosPostres);
+        String[] nombres=getResources().getStringArray(R.array.postresDescripcion);
         String[] precios=getResources().getStringArray(R.array.postresPrecio);
         String [] fotos=getResources().getStringArray(R.array.postresNombreFoto);
-        ArrayList<Producto> productos=new ArrayList<Producto>();
+        productos=new ArrayList<Producto>();
 
-        for(int i=0;i<descripciones.length;i++){
-            productos.add(new Producto(i,descripciones[i],precios[i], fotos[i]));
+        for(int i=0;i<nombres.length;i++){
+            productos.add(new Producto(i,codPostres[i],nombres[i],Double.valueOf(precios[i]), fotos[i]));
 
         }
         return productos;
@@ -65,24 +87,63 @@ public class ActivityPostres extends Activity {
 
     private void crearAdapterPostres(){
         ListView lv=(ListView) findViewById(R.id.lvListaProductos);
-        ArrayList<Producto> productos=obtenerPostres();
+        productos=obtenerPostres();
         ProductoAdapter adapter=new ProductoAdapter(this,productos);
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Producto productoElegido = (Producto)parent.getItemAtPosition(position);
-                String texto = "Has elegido: "+productoElegido.getDescripcion();
+                String texto = "Has elegido: "+productoElegido.getNombre();
                 Toast toast = Toast.makeText(ActivityPostres.this, texto, Toast.LENGTH_LONG);
                 toast.show();
 
-                Pedido pedido = new Pedido();
-                pedido.setMesa(SingletonMesa.getInstance().getMesa());
-                pedido.setNombreProducto(productoElegido.getDescripcion());
-                pedido.setPrecio(Double.valueOf(productoElegido.getPrecio()));
-                conexion.addPedido(pedido);
+                String sendText= COD_ENVIO+":"+SingletonMesa.getInstance().getMesa()+":"+productoElegido.toString();
+                try {
+                    dos.writeUTF(sendText);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
 
             }
         });
+    }
+    public class ComidaTask extends AsyncTask<Void, String, Void> {
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                socket = new Socket(ipServidor, puerto);
+                publishProgress("Conectado con el servidor");
+                dis = new DataInputStream(socket.getInputStream());
+                dos = new DataOutputStream(socket.getOutputStream());
+                while (puerto != 1) {
+                    String msg = dis.readUTF();
+                    publishProgress(msg);
+                }
+                dis.close();
+                dos.flush();
+                dos.close();
+                socket.close();
+            } catch (IOException e) {
+                Log.e(LOGCAT, "No se pudo conectar");
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
